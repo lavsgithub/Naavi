4603071d25bc083963da1b3daa570a4512aecac2
lavsgithub <137688652+lavsgithub@users.noreply.github.com>
2024-06-12 16:32:10 +0530

    path.py

--- path.py
+++ path.py
@@ -1,131 +1,108 @@
 import streamlit as st
-import replicate
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import LlamaCpp
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.document_loaders import PyPDFLoader
 import os
+import tempfile
 
-# App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
 
-# Replicate Credentials
-with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
 
-    st.subheader('Models and parameters')
-    selected_model = st.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.slider('max_length', min_value=32, max_value=4096, value=2048, step=32)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey! 👋"]
 
-# Initial questions
-questions = [
-    "How may I assist you today?",
-    "Please provide your general information like name, city, state, country.",
-    "Please provide your academic performance (grade, board, present percentage).",
-    "What is your goal, financial position, and which places are you interested in?"
-]
+def conversation_chat(query, chain, history):
+    result = chain({"question": query, "chat_history": history})
+    history.append((query, result["answer"]))
+    return result["answer"]
 
-# Options to present after initial questions
-options = [
-    "Would you like a detailed roadmap to achieve your career goals considering your academics, financial status, and study locations?",
-    "Do you want personalized career guidance based on your academic performance, financial status, and desired study locations?",
-    "Do you need other specific guidance like scholarship opportunities, study programs, or financial planning?",
-    "Other"
-]
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
 
-# Store LLM generated responses and initial questions status
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": questions[0]}]
-if "question_index" not in st.session_state:
-    st.session_state.question_index = 0
-if "answers" not in st.session_state:
-    st.session_state.answers = []
-if "show_options" not in st.session_state:
-    st.session_state.show_options = False
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Question:", placeholder="Ask about your PDF", key='input')
+            submit_button = st.form_submit_button(label='Send')
 
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+        if submit_button and user_input:
+            with st.spinner('Generating response...'):
+                output = conversation_chat(user_input, chain, st.session_state['history'])
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": questions[0]}]
-    st.session_state.question_index = 0
-    st.session_state.answers = []
-    st.session_state.show_options = False
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
 
-# Function for generating LLaMA2 response
-def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
-        else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run(
-        llm, 
-        input={
-            "prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-            "temperature": temperature,
-            "top_p": top_p,
-            "max_length": max_length,
-            "repetition_penalty": 1
-        }
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+def create_conversational_chain(vector_store):
+    # Create llm
+    llm = LlamaCpp(
+        streaming=True,
+        model_path=r"C:\Users\chint\Downloads\mistral-7b-instruct-v0.1.Q4_K_S.gguf",  # Use the correct path
+        temperature=0.75,
+        top_p=1,
+        verbose=True,
+        n_ctx=4096
     )
-    return output
+    
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
-# Function to handle user responses and provide questions/options
-def handle_user_input(user_input):
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    st.session_state.answers.append(user_input)
-    if st.session_state.question_index < len(questions) - 1:
-        st.session_state.question_index += 1
-        next_question = questions[st.session_state.question_index]
-        st.session_state.messages.append({"role": "assistant", "content": next_question})
-    else:
-        st.session_state.show_options = True
-        st.session_state.messages.append({"role": "assistant", "content": "Thank you for the information. Here are some options for further assistance:"})
-        for option in options:
-            st.session_state.messages.append({"role": "assistant", "content": option})
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
+                                                  retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
+                                                  memory=memory)
+    return chain
 
-# User-provided prompt
-if prompt := st.chat_input(disabled=not replicate_api):
-    handle_user_input(prompt)
+def main():
+    # Initialize session state
+    initialize_session_state()
+    st.title("Multi-PDF ChatBot using Mistral-7B-Instruct :books:")
+    # Initialize Streamlit
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader("Upload files", accept_multiple_files=True)
 
-# Display options and handle selection
-if st.session_state.show_options:
-    selected_option = st.selectbox("Select an option for further assistance:", options)
-    if st.button("Submit"):
-        st.session_state.messages.append({"role": "user", "content": selected_option})
-        response_context = "\n".join([f"{questions[i]} {st.session_state.answers[i]}" for i in range(len(questions))])
-        response_prompt = f"You have the following information:\n{response_context}\nBased on this information, {selected_option.lower()}"
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = generate_llama2_response(response_prompt)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
-                    placeholder.markdown(full_response)
-                placeholder.markdown(full_response)
-        message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
-        st.session_state.show_options = False
+    if uploaded_files:
+        text = []
+        for file in uploaded_files:
+            file_extension = os.path.splitext(file.name)[1]
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(file.read())
+                temp_file_path = temp_file.name
 
-# Initial prompt to ask the first question if it's the beginning of the conversation
-if st.session_state.question_index == 0 and not st.session_state.answers:
-    st.session_state.messages.append({"role": "assistant", "content": questions[0]})
+            loader = None
+            if file_extension == ".pdf":
+                loader = PyPDFLoader(temp_file_path)
+
+            if loader:
+                text.extend(loader.load())
+                os.remove(temp_file_path)
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=20)
+        text_chunks = text_splitter.split_documents(text)
+
+        # Create embeddings
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", 
+                                           model_kwargs={'device': 'cpu'})
+
+        # Create vector store
+        vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+
+        # Create the chain object
+        chain = create_conversational_chain(vector_store)
+
+        display_chat_history(chain)
+
+if __name__ == "__main__":
+    main()
